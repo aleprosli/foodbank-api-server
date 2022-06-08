b5d45e4896e1e8ca2f1a5a0845bb91d10aa0dda8
aliff <aliff.rosli96@gmail.com>
2022-06-09 02:58:40 +0800

    add category seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categories')->insert([
+            [
+                'id' => 1,
+                'name' => 'Bantuan Banjir',
+                'image' => 'Duit.jpg',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'id' => 2,
+                'name' => 'Bantuan Fakir Miskin',
+                'image' => 'Duit.png',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'id' => 3,
+                'name' => 'Bantuan Jalanan',
+                'image' => 'Duit.jpeg',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Database\Seeders\CategorySeeder;
 use Database\Seeders\FoodbankSeeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -16,5 +17,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([FoodbankSeeder::class]);
+        $this->call([CategorySeeder::class]);
     }
 }
