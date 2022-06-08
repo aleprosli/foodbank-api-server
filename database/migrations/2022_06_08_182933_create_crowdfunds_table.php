70ea1242b43e46e27ce098c17091b70e1b9a3178
aliff <aliff.rosli96@gmail.com>
2022-06-09 02:45:03 +0800

    create category and crowdfund migration and model

---
+++ database/migrations/2022_06_08_182933_create_crowdfunds_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('crowdfunds', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->nullable();
+            $table->string('description')->nullable();
+            $table->string('image')->nullable();
+            $table->unsignedInteger('category_id')->nullable();
+            $table->decimal('target',5,2)->nullable();
+            $table->string('gps_location')->nullable();
+            $table->timestamps();
+
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('crowdfunds');
+    }
+};
