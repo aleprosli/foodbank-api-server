03b5f3e068c3b2899b97a8a8812254a7c97b1160
aliff <aliff.rosli96@gmail.com>
2022-06-09 03:48:58 +0800

    add function donorpaycrowdfund , listTargetcompleted, listtargetuncomplete

--- app/Http/Controllers/API/User/CrowdfundController.php
+++ app/Http/Controllers/API/User/CrowdfundController.php
@@ -5,10 +5,13 @@ namespace App\Http\Controllers\API\User;
 use App\Models\Crowdfund;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use File;
+use Storage;
 
 class CrowdfundController extends Controller
 {
-    public function list()
+    public function listAll()
     {
         try {
             $crowdfund = Crowdfund::all();
@@ -23,6 +26,39 @@ class CrowdfundController extends Controller
             return response()->error($e->getMessage(),true);
         }
     }
+
+    public function listTargetCompleted(Request $request)
+    {
+        try {
+            $crowdfund = Crowdfund::where('target',0)->get();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Successfully fetch crowdfund list completed',
+                'data' => $crowdfund,
+            ]);
+        } 
+        catch (Exception $e) {
+            return response()->error($e->getMessage(),true);
+        }
+    }
+
+    public function listTargetUncomplete(Request $request)
+    {
+        try {
+            $crowdfund = Crowdfund::where('target','>',0)->get();
+
+            return response()->json([
+                'success' => true,
+                'message' => 'Successfully fetch crowdfund list uncompleted',
+                'data' => $crowdfund,
+            ]);
+        } 
+        catch (Exception $e) {
+            return response()->error($e->getMessage(),true);
+        }
+    }
+
     public function createCrowdfund(Request $request)
     {
         try {
@@ -34,11 +70,64 @@ class CrowdfundController extends Controller
                 'gps_location' => $request->gps_location,
             ]);
 
+            if($request->hasFile('image')){
+                $filename = $crowdfund->title.'-'.date("d-m-Y").'.'.$request->image->getClientOriginalExtension();
+
+                Storage::disk('public')->put($filename, File::get($request->image));
+ 
+                $crowdfund->image = $filename;
+    
+                $crowdfund->save();
+            }
+
             return response()->json([
                 'success' => true,
                 'message' => 'Crowdfund created with target RM'.$crowdfund->target,
                 'data' => $crowdfund,
             ]);
+        } 
+        catch (Exception $e) {
+            return response()->error($e->getMessage(),true);
+        }
+    }
+
+    public function donorPayCrowdfund(Request $request, Crowdfund $crowdfund)
+    {
+        try {
+            if(Auth::user()->is_donor == 1)
+            {
+                $donorPay = $request->priceDonate;
+
+                $newTargetPrice = $crowdfund->target - $donorPay;
+    
+                if($newTargetPrice > -1)
+                {
+                    $crowdfund->update([
+                        'target' => $newTargetPrice,
+                    ]);
+                }
+                else
+                {
+                    return response()->json([
+                        'success' => false,
+                        'message' => 'You donation exceed our target. Maximum donation amount we can accept is RM '.$crowdfund->target,
+                        'data' => $crowdfund,
+                    ]);
+                }
+                
+                return response()->json([
+                    'success' => true,
+                    'message' => 'Thank you for your donation '.Auth::user()->name,
+                    'data' => $crowdfund,
+                ]);
+            }
+            else
+            {
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Wrong identity. Only donor can donate this crowdfund',
+                ]);
+            }
         } 
         catch (Exception $e) {
             return response()->error($e->getMessage(),true);
