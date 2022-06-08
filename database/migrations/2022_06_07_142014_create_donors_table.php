c8d5300651e3020ced808c5b736a48ffb4fca955
aliff <aliff.rosli96@gmail.com>
2022-06-08 22:44:34 +0800

    update donors table

--- database/migrations/2022_06_07_142014_create_donors_table.php
+++ database/migrations/2022_06_07_142014_create_donors_table.php
@@ -16,10 +16,12 @@ return new class extends Migration
         Schema::create('donors', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('user_id')->nullable();
+            $table->unsignedInteger('level_id')->nullable();
             $table->string('image')->nullable();
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('level_id')->references('id')->on('levels')->onDelete('cascade');
         });
     }
 
