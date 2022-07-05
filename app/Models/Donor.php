9b411a1fef076649c1c92522ca03921005feaf1c
aliff <aliff.rosli96@gmail.com>
2022-07-06 00:20:26 +0800

    update model, add fk for level

--- app/Models/Donor.php
+++ app/Models/Donor.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\User;
+use App\Models\Level;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -13,11 +14,18 @@ class Donor extends Model
     protected $fillable = [
         'user_id',
         'image',
-        'donation_count'
+        'donation_count',
+        'level_id'
     ];
 
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    public function level()
+    {
+        return $this->belongsTo(Level::class, 'level_id');
+    }
+
 }
