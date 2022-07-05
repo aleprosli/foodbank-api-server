204c796fbd58b0d2faaa5a552c4ae10e24b3cf2b
aliff <aliff.rosli96@gmail.com>
2022-07-06 00:21:01 +0800

    create level seeder

---
+++ database/seeders/LevelSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class LevelSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('levels')->insert([
+            [
+                'id' => 1,
+                'title' => 'Level 1',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'id' => 2,
+                'title' => 'Level 2',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'id' => 3,
+                'title' => 'Level 3',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'id' => 4,
+                'title' => 'Level 4',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            [
+                'id' => 5,
+                'title' => 'Level 5',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            ]);
+    }
+}
