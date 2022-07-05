4b0f791aedab1aa8ba527b5e31b70a189a9d39c7
aliff <aliff.rosli96@gmail.com>
2022-07-06 00:20:42 +0800

    add fk relation for level

--- app/Models/Level.php
+++ app/Models/Level.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Donor;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Level extends Model
 {
@@ -12,4 +13,9 @@ class Level extends Model
     protected $fillable = [
         'total',
     ];
+
+    public function donor()
+    {
+        return $this->hasOne(Donor::class);
+    }
 }
