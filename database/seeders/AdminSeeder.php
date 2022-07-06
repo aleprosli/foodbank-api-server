1fe52567ca0f4cc2ae968c7740174a15f1b5c9f6
aliff <aliff.rosli96@gmail.com>
2022-07-07 00:59:09 +0800

    create admin seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'id' => 1,
+                'name' => 'Admin',
+                'email' => 'Admin@gmail.com',
+                'password' => Hash::make('password'),
+                'is_admin' => 1,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ]
+            ]);
+    }
+}
