59b3b3a36271cee4afb325e8d63fae68ad0dde39
aliff <aliff.rosli96@gmail.com>
2022-07-07 00:59:26 +0800

    create crowdfund

---
+++ database/seeders/CrowdfundSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Carbon;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CrowdfundSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('crowdfunds')->insert([
+            [
+                'id' => 1,
+                'user_id' => 1,
+                'title' => 'Emergency Funds',
+                'image' => 'Duit.jpg',
+                'description' => 'For Emergency Funds',
+                'category_id' => 4,
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s'),
+            ],
+            ]);
+    }
+}
